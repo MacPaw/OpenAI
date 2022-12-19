3c31afd1e824d23069b2808b040734647d2d036f
Serg Krivoblotsky <kri.sergy@gmail.com>
2022-12-19 14:11:34 +0200

    Add completions documentation

--- Sources/OpenAI/OpenAI.swift
+++ Sources/OpenAI/OpenAI.swift
@@ -26,13 +26,21 @@ final public class OpenAI {
 public extension OpenAI {
     
     struct CompletionsQuery: Codable {
+        /// ID of the model to use.
         let model: Model
+        /// The prompt(s) to generate completions for, encoded as a string, array of strings, array of tokens, or array of token arrays.
         let prompt: String
+        /// What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer.
         let temperature: Int
+        /// The maximum number of tokens to generate in the completion.
         let max_tokens: Int
+        /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
         let top_p: Int
+        /// Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
         let frequency_penalty: Int
+        /// Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
         let presence_penalty: Int
+        /// Up to 4 sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence.
         let stop: [String]
     }
     
