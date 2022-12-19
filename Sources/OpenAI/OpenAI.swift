20df14640487eca3eff40e74f4acecc37ed09b98
Serg Krivoblotsky <kri.sergy@gmail.com>
2022-12-19 19:22:12 +0200

    Fix visibility

--- Sources/OpenAI/OpenAI.swift
+++ Sources/OpenAI/OpenAI.swift
@@ -27,34 +27,45 @@ public extension OpenAI {
     
     struct CompletionsQuery: Codable {
         /// ID of the model to use.
-        let model: Model
+        public let model: Model
         /// The prompt(s) to generate completions for, encoded as a string, array of strings, array of tokens, or array of token arrays.
-        let prompt: String
+        public let prompt: String
         /// What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer.
-        let temperature: Int
+        public let temperature: Int
         /// The maximum number of tokens to generate in the completion.
-        let max_tokens: Int
+        public let max_tokens: Int
         /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
-        let top_p: Int
+        public let top_p: Int
         /// Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-        let frequency_penalty: Int
+        public let frequency_penalty: Int
         /// Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-        let presence_penalty: Int
+        public let presence_penalty: Int
         /// Up to 4 sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence.
-        let stop: [String]
+        public let stop: [String]
+        
+        public init(model: Model, prompt: String, temperature: Int, max_tokens: Int, top_p: Int, frequency_penalty: Int, presence_penalty: Int, stop: [String]) {
+            self.model = model
+            self.prompt = prompt
+            self.temperature = temperature
+            self.max_tokens = max_tokens
+            self.top_p = top_p
+            self.frequency_penalty = frequency_penalty
+            self.presence_penalty = presence_penalty
+            self.stop = stop
+        }
     }
     
     struct CompletionsResult: Codable {
-        struct Choice: Codable {
+        public struct Choice: Codable {
             let text: String
             let index: Int
         }
 
-        let id: String
-        let object: String
-        let created: TimeInterval
-        let model: Model
-        let choices: [Choice]
+        public let id: String
+        public let object: String
+        public let created: TimeInterval
+        public let model: Model
+        public let choices: [Choice]
     }
 
     func completions(query: CompletionsQuery, completion: @escaping (Result<CompletionsResult, Error>) -> Void) {
@@ -67,19 +78,25 @@ public extension OpenAI {
     
     struct ImagesQuery: Codable {
         /// A text description of the desired image(s). The maximum length is 1000 characters.
-        let prompt: String
+        public let prompt: String
         /// The number of images to generate. Must be between 1 and 10.
-        let n: Int?
+        public let n: Int?
         /// The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024.
-        let size: String?
+        public let size: String?
+        
+        public init(prompt: String, n: Int?, size: String?) {
+            self.prompt = prompt
+            self.n = n
+            self.size = size
+        }
     }
     
     struct ImagesResult: Codable {
-        struct URLResult: Codable {
+        public struct URLResult: Codable {
             let url: String
         }
-        let created: TimeInterval
-        let data: [URLResult]
+        public let created: TimeInterval
+        public let data: [URLResult]
     }
     
     func images(query: ImagesQuery, completion: @escaping (Result<ImagesResult, Error>) -> Void) {
@@ -92,20 +109,24 @@ public extension OpenAI {
     
     struct EmbeddingsQuery: Codable {
         /// ID of the model to use.
-        let model: Model
+        public let model: Model
         /// Input text to get embeddings for
-        let input: String
+        public let input: String
+        
+        public init(model: Model, input: String) {
+            self.model = model
+            self.input = input
+        }
     }
     
     struct EmbeddingsResult: Codable {
         
-        struct Embedding: Codable {
-            
-            let object: String
-            let embedding: [Double]
-            let index: Int
+        public struct Embedding: Codable {
+            public let object: String
+            public let embedding: [Double]
+            public let index: Int
         }
-        let data: [Embedding]
+        public let data: [Embedding]
     }
     
     func embeddings(query: EmbeddingsQuery, completion: @escaping (Result<EmbeddingsResult, Error>) -> Void) {
