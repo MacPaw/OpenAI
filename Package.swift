066f469f0088fba214ad6d6a9a8e33f5c45f5cae
Serg Krivoblotsky <kri.sergy@gmail.com>
2023-02-12 16:53:09 +0200

    Update for all platforms

--- Package.swift
+++ Package.swift
@@ -1,17 +1,10 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "OpenAI",
-    platforms: [
-        .macOS(.v10_13),
-        .iOS(.v11),
-        .watchOS(.v6),
-        .tvOS(.v11),
-            .macCatalyst(.v13)
-    ],
     products: [
         .library(
             name: "OpenAI",
