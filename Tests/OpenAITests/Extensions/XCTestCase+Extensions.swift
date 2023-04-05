ae61cd60dd0f3f59544bb1f2dc19a11f4bdde3b7
Serg Krivoblotsky <kri.sergy@gmail.com>
2023-04-05 11:16:25 +0300

    Fix watchOS build for Swift 5.8

--- Tests/OpenAITests/Extensions/XCTestCase+Extensions.swift
+++ Tests/OpenAITests/Extensions/XCTestCase+Extensions.swift
@@ -11,6 +11,7 @@ import XCTest
 import Combine
 
 //Borrowed from here: https://www.swiftbysundell.com/articles/unit-testing-combine-based-swift-code/
+@available(watchOS 6.0, *)
 @available(tvOS 13.0, *)
 @available(iOS 13.0, *)
 extension XCTestCase {
