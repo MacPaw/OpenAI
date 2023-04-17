8d7cd667255016b92f72e81bebe21517bc57e12e
Sihao Lu <lsh32768@gmail.com>
2023-04-16 17:48:36 -0700

    Make a few session-related types and initializers public

--- Sources/OpenAI/OpenAI.swift
+++ Sources/OpenAI/OpenAI.swift
@@ -45,10 +45,14 @@ final public class OpenAI: OpenAIProtocol {
     public convenience init(configuration: Configuration) {
         self.init(configuration: configuration, session: URLSession.shared)
     }
-    
-    init(configuration: Configuration, session: URLSessionProtocol = URLSession.shared) {
+
+    init(configuration: Configuration, session: URLSessionProtocol) {
         self.configuration = configuration
         self.session = session
+    }
+
+    public convenience init(configuration: Configuration, session: URLSession = URLSession.shared) {
+        self.init(configuration: configuration, session: session as URLSessionProtocol)
     }
     
     public func completions(query: CompletionsQuery, completion: @escaping (Result<CompletionsResult, Error>) -> Void) {
