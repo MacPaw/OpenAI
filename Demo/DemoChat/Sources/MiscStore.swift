11a31932658779b1fb376c17e977898e29d62867
Aled Samuel <sunburstenzo@gmail.com>
2023-04-26 17:36:53 +0100

    Add Moderations chat example to Demo app

--- Demo/DemoChat/Sources/MiscStore.swift
+++ Demo/DemoChat/Sources/MiscStore.swift
@@ -19,6 +19,8 @@ public final class MiscStore: ObservableObject {
         self.openAIClient = openAIClient
     }
     
+    // MARK: Models
+    
     @MainActor
     func getModels() async {
         do {
@@ -27,6 +29,66 @@ public final class MiscStore: ObservableObject {
         } catch {
             // TODO: Better error handling
             print(error.localizedDescription)
+        }
+    }
+    
+    // MARK: Moderations
+    
+    @Published var moderationConversation = Conversation(id: "", messages: [])
+    @Published var moderationConversationError: Error?
+
+    @MainActor
+    func sendModerationMessage(_ message: Message) async {
+        moderationConversation.messages.append(message)
+        await completeModerationChat(message: message)
+    }
+    
+    @MainActor
+    func completeModerationChat(message: Message) async {
+        
+        moderationConversationError = nil
+        
+        do {
+            let response = try await openAIClient.moderations(
+                query: ModerationsQuery(
+                    input: message.content,
+                    model: .textModerationLatest
+                )
+            )
+            
+            let categoryResults = response.results
+            
+            let existingMessages = moderationConversation.messages
+            
+            func circleEmoji(for resultType: Bool) -> String {
+                resultType ? "🔴" : "🟢"
+            }
+            
+            for result in categoryResults {
+                let content = """
+                \(circleEmoji(for: result.categories.hate)) Hate
+                \(circleEmoji(for: result.categories.hateThreatening)) Hate/Threatening
+                \(circleEmoji(for: result.categories.selfHarm)) Self-harm
+                \(circleEmoji(for: result.categories.sexual)) Sexual
+                \(circleEmoji(for: result.categories.sexualMinors)) Sexual/Minors
+                \(circleEmoji(for: result.categories.violence)) Violence
+                \(circleEmoji(for: result.categories.violenceGraphic)) Violence/Graphic
+                """
+                
+                let message = Message(
+                    id: response.id,
+                    role: .assistant,
+                    content: content,
+                    createdAt: message.createdAt)
+                
+                if existingMessages.contains(message) {
+                    continue
+                }
+                moderationConversation.messages.append(message)
+            }
+            
+        } catch {
+            moderationConversationError = error
         }
     }
 }
---
+++ Demo/DemoChat/Sources/UI/ModerationChatView.swift
@@ -0,0 +1,38 @@
+//
+//  ModerationChatView.swift
+//  DemoChat
+//
+//  Created by Aled Samuel on 26/04/2023.
+//
+
+import SwiftUI
+
+public struct ModerationChatView: View {
+    @ObservedObject var store: MiscStore
+    
+    @Environment(\.dateProviderValue) var dateProvider
+    @Environment(\.idProviderValue) var idProvider
+
+    public init(store: MiscStore) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        DetailView(
+            conversation: store.moderationConversation,
+            error: store.moderationConversationError,
+            sendMessage: { message in
+                Task {
+                    await store.sendModerationMessage(
+                        Message(
+                            id: idProvider(),
+                            role: .user,
+                            content: message,
+                            createdAt: dateProvider()
+                        )
+                    )
+                }
+            }
+        )
+    }
+}
