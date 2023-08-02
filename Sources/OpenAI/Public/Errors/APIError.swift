56dd35ca36a0e26568a3d884e5a53296ded3f60d
fabstu <fabian.sturm@aduu.de>
2023-08-02 13:16:43 +0200

    APIErrorResponse message can be [String]. Handle it.

--- Sources/OpenAI/Public/Errors/APIError.swift
+++ Sources/OpenAI/Public/Errors/APIError.swift
@@ -16,6 +16,39 @@ public struct APIError: Error, Decodable, Equatable {
     public let type: String
     public let param: String?
     public let code: String?
+  
+  public init(message: String, type: String, param: String?, code: String?) {
+    self.message = message
+    self.type = type
+    self.param = param
+    self.code = code
+  }
+  
+  enum CodingKeys: CodingKey {
+    case message
+    case type
+    case param
+    case code
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    
+    //
+    // message can be String or [String].
+    //
+    if let string = try? container.decode(String.self, forKey: .message) {
+      self.message = string
+    } else if let array = try? container.decode([String].self, forKey: .message) {
+      self.message = array.joined(separator: "\n")
+    } else {
+      throw DecodingError.typeMismatch(String.self, .init(codingPath: [CodingKeys.message], debugDescription: "message: expected String or [String]"))
+    }
+    
+    self.type = try container.decode(String.self, forKey: .type)
+    self.param = try container.decodeIfPresent(String.self, forKey: .param)
+    self.code = try container.decodeIfPresent(String.self, forKey: .code)
+  }
 }
 
 extension APIError: LocalizedError {
