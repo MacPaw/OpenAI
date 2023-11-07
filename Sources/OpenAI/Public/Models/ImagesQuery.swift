d3213da8f722f2ddc189742e7dcf671ba3b229f1
rawnly <rawnly@users.noreply.github.com>
2023-11-07 21:46:32 +0100

    fix: added model option
    Signed-off-by: rawnly <rawnly@users.noreply.github.com>

--- Sources/OpenAI/Public/Models/ImagesQuery.swift
+++ Sources/OpenAI/Public/Models/ImagesQuery.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 public struct ImagesQuery: Codable {
+    /// ID of the model to use. 
+    public let model: Model
+
     /// A text description of the desired image(s). The maximum length is 1000 characters.
     public let prompt: String
     /// The number of images to generate. Must be between 1 and 10.
@@ -15,9 +18,10 @@ public struct ImagesQuery: Codable {
     /// The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024.
     public let size: String?
 
-    public init(prompt: String, n: Int?, size: String?) {
+    public init(prompt: String, model: Model = .dall_e_2, n: Int?, size: String?) {
         self.prompt = prompt
         self.n = n
         self.size = size
+        self.model = model
     }
 }
