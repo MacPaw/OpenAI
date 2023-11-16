89040bdbe24557ef865094a69e34be3520b6dae7
antswing <851348654@qq.com>
2023-11-16 21:55:49 +0800

    add aoai

---
+++ Sources/OpenAI/AzureOpenAI.swift
@@ -0,0 +1,221 @@
+//
+//  OpenAI.swift
+//
+//
+//  Created by Sergii Kryvoblotskyi on 9/18/22.
+//
+
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+final public class AzureOpenAI: OpenAIProtocol {
+
+    public struct Configuration {
+        
+        /// OpenAI API token. See https://platform.openai.com/docs/api-reference/authentication
+        public let token: String
+        
+        /// Optional OpenAI organization identifier. See https://platform.openai.com/docs/api-reference/authentication
+        public let organizationIdentifier: String?
+        
+        /// API host. Set this property if you use some kind of proxy or your own server. Default is api.openai.com
+        public let host: String
+
+        /// model name.
+        public let model: String
+
+        /// API version.
+        public let apiVersion: String
+        
+        /// Default request timeout
+        public let timeoutInterval: TimeInterval
+        
+        public init(token: String, organizationIdentifier: String? = nil, host: String = "api.openai.com", model: String = "", apiVersion: String = "", timeoutInterval: TimeInterval = 60.0) {
+            self.token = token
+            self.organizationIdentifier = organizationIdentifier
+            self.host = host
+            self.model = model
+            self.apiVersion = apiVersion
+            self.timeoutInterval = timeoutInterval
+        }
+    }
+    
+    private let session: URLSessionProtocol
+    private var streamingSessions: [NSObject] = []
+    
+    public let configuration: Configuration
+
+    public convenience init(apiToken: String, host: String, model: String, apiVersion: String) {
+        self.init(configuration: Configuration(token: apiToken, host: host, model: model, apiVersion: apiVersion), session: URLSession.shared)
+    }
+    
+    public convenience init(configuration: Configuration) {
+        self.init(configuration: configuration, session: URLSession.shared)
+    }
+
+    init(configuration: Configuration, session: URLSessionProtocol) {
+        self.configuration = configuration
+        self.session = session
+    }
+
+    public convenience init(configuration: Configuration, session: URLSession = URLSession.shared) {
+        self.init(configuration: configuration, session: session as URLSessionProtocol)
+    }
+    
+    public func completions(query: CompletionsQuery, completion: @escaping (Result<CompletionsResult, Error>) -> Void) {
+        performRequest(request: JSONRequest<CompletionsResult>(body: query, url: buildURL(path: .completionsAOAI)), completion: completion)
+    }
+    
+    public func completionsStream(query: CompletionsQuery, onResult: @escaping (Result<CompletionsResult, Error>) -> Void, completion: ((Error?) -> Void)?) {
+        performSteamingRequest(request: JSONRequest<CompletionsResult>(body: query.makeStreamable(), url: buildURL(path: .completionsAOAI)), onResult: onResult, completion: completion)
+    }
+    
+    public func images(query: ImagesQuery, completion: @escaping (Result<ImagesResult, Error>) -> Void) {
+        performRequest(request: JSONRequest<ImagesResult>(body: query, url: buildURL(path: .imagesAOAI)), completion: completion)
+    }
+    
+    public func imageEdits(query: ImageEditsQuery, completion: @escaping (Result<ImagesResult, Error>) -> Void) {
+        performRequest(request: MultipartFormDataRequest<ImagesResult>(body: query, url: buildURL(path: .imageEditsAOAI)), completion: completion)
+    }
+    
+    public func imageVariations(query: ImageVariationsQuery, completion: @escaping (Result<ImagesResult, Error>) -> Void) {
+        performRequest(request: MultipartFormDataRequest<ImagesResult>(body: query, url: buildURL(path: .imageVariationsAOAI)), completion: completion)
+    }
+    
+    public func embeddings(query: EmbeddingsQuery, completion: @escaping (Result<EmbeddingsResult, Error>) -> Void) {
+        performRequest(request: JSONRequest<EmbeddingsResult>(body: query, url: buildURL(path: .embeddingsAOAI)), completion: completion)
+    }
+    
+    public func chats(query: ChatQuery, completion: @escaping (Result<ChatResult, Error>) -> Void) {
+        performRequest(request: JSONRequest<ChatResult>(body: query, url: buildURL(path: .chatsAOAI)), completion: completion)
+    }
+    
+    public func chatsStream(query: ChatQuery, onResult: @escaping (Result<ChatStreamResult, Error>) -> Void, completion: ((Error?) -> Void)?) {
+        performSteamingRequest(request: JSONRequest<ChatResult>(body: query.makeStreamable(), url: buildURL(path: .chatsAOAI)), onResult: onResult, completion: completion)
+    }
+    
+    public func edits(query: EditsQuery, completion: @escaping (Result<EditsResult, Error>) -> Void) {
+        performRequest(request: JSONRequest<EditsResult>(body: query, url: buildURL(path: .editsAOAI)), completion: completion)
+    }
+    
+    public func model(query: ModelQuery, completion: @escaping (Result<ModelResult, Error>) -> Void) {
+        performRequest(request: JSONRequest<ModelResult>(url: buildURL(path: .modelsAOAI.withPath(query.model)), method: "GET"), completion: completion)
+    }
+    
+    public func models(completion: @escaping (Result<ModelsResult, Error>) -> Void) {
+        performRequest(request: JSONRequest<ModelsResult>(url: buildURL(path: .modelsAOAI), method: "GET"), completion: completion)
+    }
+    
+    public func moderations(query: ModerationsQuery, completion: @escaping (Result<ModerationsResult, Error>) -> Void) {
+        performRequest(request: JSONRequest<ModerationsResult>(body: query, url: buildURL(path: .moderationsAOAI)), completion: completion)
+    }
+    
+    public func audioTranscriptions(query: AudioTranscriptionQuery, completion: @escaping (Result<AudioTranscriptionResult, Error>) -> Void) {
+        performRequest(request: MultipartFormDataRequest<AudioTranscriptionResult>(body: query, url: buildURL(path: .audioTranscriptionsAOAI)), completion: completion)
+    }
+    
+    public func audioTranslations(query: AudioTranslationQuery, completion: @escaping (Result<AudioTranslationResult, Error>) -> Void) {
+        performRequest(request: MultipartFormDataRequest<AudioTranslationResult>(body: query, url: buildURL(path: .audioTranslationsAOAI)), completion: completion)
+    }
+}
+
+extension AzureOpenAI {
+
+    func performRequest<ResultType: Codable>(request: any URLRequestBuildable, completion: @escaping (Result<ResultType, Error>) -> Void) {
+        do {
+            let request = try request.build(token: configuration.token, organizationIdentifier: configuration.organizationIdentifier, timeoutInterval: configuration.timeoutInterval)
+            let task = session.dataTask(with: request) { data, _, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                guard let data = data else {
+                    completion(.failure(OpenAIError.emptyData))
+                    return
+                }
+
+                var apiError: Error? = nil
+                do {
+                    let decoded = try JSONDecoder().decode(ResultType.self, from: data)
+                    completion(.success(decoded))
+                } catch {
+                    apiError = error
+                }
+
+                if let apiError = apiError {
+                    do {
+                        let decoded = try JSONDecoder().decode(APIErrorResponse.self, from: data)
+                        completion(.failure(decoded))
+                    } catch {
+                        completion(.failure(apiError))
+                    }
+                }
+            }
+            task.resume()
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    func performSteamingRequest<ResultType: Codable>(request: any URLRequestBuildable, onResult: @escaping (Result<ResultType, Error>) -> Void, completion: ((Error?) -> Void)?) {
+        do {
+            let request = try request.build(token: configuration.token, organizationIdentifier: configuration.organizationIdentifier, timeoutInterval: configuration.timeoutInterval)
+            let session = StreamingSession<ResultType>(urlRequest: request)
+            session.onReceiveContent = {_, object in
+                onResult(.success(object))
+            }
+            session.onProcessingError = {_, error in
+                onResult(.failure(error))
+            }
+            session.onComplete = { [weak self] object, error in
+                self?.streamingSessions.removeAll(where: { $0 == object })
+                completion?(error)
+            }
+            session.perform()
+            streamingSessions.append(session)
+        } catch {
+            completion?(error)
+        }
+    }
+}
+
+extension AzureOpenAI {
+    
+    func buildURL(path: String) -> URL {
+        
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = configuration.host
+        components.path = "/openai/deployments/\(self.configuration.model)/\(path)"
+        components.queryItems = [
+            URLQueryItem(name: "api-version", value: self.configuration.apiVersion),
+        ]
+        print("components.url: \(components.url!)")
+        return components.url!
+    }
+}
+
+typealias AzureOpenAPIPath = String
+extension AzureOpenAPIPath {
+    
+    static let completionsAOAI = "completions"
+    static let embeddingsAOAI = "embeddings"
+    static let chatsAOAI = "chat/completions" //"/openai/deployments/gpt-35-turbo-0301/chat/completions?api-version=2023-03-15-preview" //"/v1/chat/completions"
+    static let editsAOAI = "edits"
+    static let modelsAOAI = "models"
+    static let moderationsAOAI = "moderations"
+    
+    static let audioTranscriptionsAOAI = "audio/transcriptions"
+    static let audioTranslationsAOAI = "audio/translations"
+    
+    static let imagesAOAI = "images/generations"
+    static let imageEditsAOAI = "images/edits"
+    static let imageVariationsAOAI = "images/variations"
+    
+    /*
+    func withPath(_ path: String) -> String {
+        self + "/" + path
+    }*/
+}
