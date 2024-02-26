a6bfbbfa3e0517ea51141385b722c0ff37909aca
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-02-26 12:30:04 -0800

    :alien: (Groq) Allow paths in model host to enable Groq API support
    Assumes that there is no trailing slash

--- Sources/OpenAI/OpenAI.swift
+++ Sources/OpenAI/OpenAI.swift
@@ -7,25 +7,23 @@
 
 import Foundation
 #if canImport(FoundationNetworking)
-import FoundationNetworking
+    import FoundationNetworking
 #endif
 
-final public class OpenAI: OpenAIProtocol {
-
+public final class OpenAI: OpenAIProtocol {
     public struct Configuration {
-        
         /// OpenAI API token. See https://platform.openai.com/docs/api-reference/authentication
         public let token: String
-        
+
         /// Optional OpenAI organization identifier. See https://platform.openai.com/docs/api-reference/authentication
         public let organizationIdentifier: String?
-        
+
         /// API host. Set this property if you use some kind of proxy or your own server. Default is api.openai.com
         public let host: String
-        
+
         /// Default request timeout
         public let timeoutInterval: TimeInterval
-        
+
         public init(token: String, organizationIdentifier: String? = nil, host: String = "api.openai.com", timeoutInterval: TimeInterval = 60.0) {
             self.token = token
             self.organizationIdentifier = organizationIdentifier
@@ -33,16 +31,16 @@ final public class OpenAI: OpenAIProtocol {
             self.timeoutInterval = timeoutInterval
         }
     }
-    
+
     private let session: URLSessionProtocol
     private var streamingSessions = ArrayWithThreadSafety<NSObject>()
-    
+
     public let configuration: Configuration
 
     public convenience init(apiToken: String) {
         self.init(configuration: Configuration(token: apiToken), session: URLSession.shared)
     }
-    
+
     public convenience init(configuration: Configuration) {
         self.init(configuration: configuration, session: URLSession.shared)
     }
@@ -55,75 +53,73 @@ final public class OpenAI: OpenAIProtocol {
     public convenience init(configuration: Configuration, session: URLSession = URLSession.shared) {
         self.init(configuration: configuration, session: session as URLSessionProtocol)
     }
-    
+
     public func completions(query: CompletionsQuery, completion: @escaping (Result<CompletionsResult, Error>) -> Void) {
         performRequest(request: JSONRequest<CompletionsResult>(body: query, url: buildURL(path: .completions)), completion: completion)
     }
-    
+
     public func completionsStream(query: CompletionsQuery, onResult: @escaping (Result<CompletionsResult, Error>) -> Void, completion: ((Error?) -> Void)?) {
         performStreamingRequest(request: JSONRequest<CompletionsResult>(body: query.makeStreamable(), url: buildURL(path: .completions)), onResult: onResult, completion: completion)
     }
-    
+
     public func images(query: ImagesQuery, completion: @escaping (Result<ImagesResult, Error>) -> Void) {
         performRequest(request: JSONRequest<ImagesResult>(body: query, url: buildURL(path: .images)), completion: completion)
     }
-    
+
     public func imageEdits(query: ImageEditsQuery, completion: @escaping (Result<ImagesResult, Error>) -> Void) {
         performRequest(request: MultipartFormDataRequest<ImagesResult>(body: query, url: buildURL(path: .imageEdits)), completion: completion)
     }
-    
+
     public func imageVariations(query: ImageVariationsQuery, completion: @escaping (Result<ImagesResult, Error>) -> Void) {
         performRequest(request: MultipartFormDataRequest<ImagesResult>(body: query, url: buildURL(path: .imageVariations)), completion: completion)
     }
-    
+
     public func embeddings(query: EmbeddingsQuery, completion: @escaping (Result<EmbeddingsResult, Error>) -> Void) {
         performRequest(request: JSONRequest<EmbeddingsResult>(body: query, url: buildURL(path: .embeddings)), completion: completion)
     }
-    
+
     public func chats(query: ChatQuery, completion: @escaping (Result<ChatResult, Error>) -> Void) {
         performRequest(request: JSONRequest<ChatResult>(body: query, url: buildURL(path: .chats)), completion: completion)
     }
-    
+
     public func chatsStream(query: ChatQuery, onResult: @escaping (Result<ChatStreamResult, Error>) -> Void, completion: ((Error?) -> Void)?) {
         performStreamingRequest(request: JSONRequest<ChatStreamResult>(body: query.makeStreamable(), url: buildURL(path: .chats)), onResult: onResult, completion: completion)
     }
-    
+
     public func edits(query: EditsQuery, completion: @escaping (Result<EditsResult, Error>) -> Void) {
         performRequest(request: JSONRequest<EditsResult>(body: query, url: buildURL(path: .edits)), completion: completion)
     }
-    
+
     public func model(query: ModelQuery, completion: @escaping (Result<ModelResult, Error>) -> Void) {
         performRequest(request: JSONRequest<ModelResult>(url: buildURL(path: .models.withPath(query.model)), method: "GET"), completion: completion)
     }
-    
+
     public func models(completion: @escaping (Result<ModelsResult, Error>) -> Void) {
         performRequest(request: JSONRequest<ModelsResult>(url: buildURL(path: .models), method: "GET"), completion: completion)
     }
-    
+
     @available(iOS 13.0, *)
     public func moderations(query: ModerationsQuery, completion: @escaping (Result<ModerationsResult, Error>) -> Void) {
         performRequest(request: JSONRequest<ModerationsResult>(body: query, url: buildURL(path: .moderations)), completion: completion)
     }
-    
+
     public func audioTranscriptions(query: AudioTranscriptionQuery, completion: @escaping (Result<AudioTranscriptionResult, Error>) -> Void) {
         performRequest(request: MultipartFormDataRequest<AudioTranscriptionResult>(body: query, url: buildURL(path: .audioTranscriptions)), completion: completion)
     }
-    
+
     public func audioTranslations(query: AudioTranslationQuery, completion: @escaping (Result<AudioTranslationResult, Error>) -> Void) {
         performRequest(request: MultipartFormDataRequest<AudioTranslationResult>(body: query, url: buildURL(path: .audioTranslations)), completion: completion)
     }
-    
+
     public func audioCreateSpeech(query: AudioSpeechQuery, completion: @escaping (Result<AudioSpeechResult, Error>) -> Void) {
         performSpeechRequest(request: JSONRequest<AudioSpeechResult>(body: query, url: buildURL(path: .audioSpeech)), completion: completion)
     }
-    
 }
 
 extension OpenAI {
-
     func performRequest<ResultType: Codable>(request: any URLRequestBuildable, completion: @escaping (Result<ResultType, Error>) -> Void) {
         do {
-            let request = try request.build(token: configuration.token, 
+            let request = try request.build(token: configuration.token,
                                             organizationIdentifier: configuration.organizationIdentifier,
                                             timeoutInterval: configuration.timeoutInterval)
             let task = session.dataTask(with: request) { data, _, error in
@@ -135,7 +131,7 @@ extension OpenAI {
                 }
                 let decoder = JSONDecoder()
                 do {
-                    completion(.success(try decoder.decode(ResultType.self, from: data)))
+                    try completion(.success(decoder.decode(ResultType.self, from: data)))
                 } catch {
                     completion(.failure((try? decoder.decode(APIErrorResponse.self, from: data)) ?? error))
                 }
@@ -145,17 +141,17 @@ extension OpenAI {
             completion(.failure(error))
         }
     }
-    
+
     func performStreamingRequest<ResultType: Codable>(request: any URLRequestBuildable, onResult: @escaping (Result<ResultType, Error>) -> Void, completion: ((Error?) -> Void)?) {
         do {
-            let request = try request.build(token: configuration.token, 
+            let request = try request.build(token: configuration.token,
                                             organizationIdentifier: configuration.organizationIdentifier,
                                             timeoutInterval: configuration.timeoutInterval)
             let session = StreamingSession<ResultType>(urlRequest: request)
-            session.onReceiveContent = {_, object in
+            session.onReceiveContent = { _, object in
                 onResult(.success(object))
             }
-            session.onProcessingError = {_, error in
+            session.onProcessingError = { _, error in
                 onResult(.failure(error))
             }
             session.onComplete = { [weak self] object, error in
@@ -168,13 +164,13 @@ extension OpenAI {
             completion?(error)
         }
     }
-    
+
     func performSpeechRequest(request: any URLRequestBuildable, completion: @escaping (Result<AudioSpeechResult, Error>) -> Void) {
         do {
-            let request = try request.build(token: configuration.token, 
+            let request = try request.build(token: configuration.token,
                                             organizationIdentifier: configuration.organizationIdentifier,
                                             timeoutInterval: configuration.timeoutInterval)
-            
+
             let task = session.dataTask(with: request) { data, _, error in
                 if let error = error {
                     return completion(.failure(error))
@@ -182,7 +178,7 @@ extension OpenAI {
                 guard let data = data else {
                     return completion(.failure(OpenAIError.emptyData))
                 }
-                
+
                 completion(.success(AudioSpeechResult(audio: data)))
             }
             task.resume()
@@ -193,34 +189,48 @@ extension OpenAI {
 }
 
 extension OpenAI {
-    
     func buildURL(path: String) -> URL {
         var components = URLComponents()
         components.scheme = "https"
-        components.host = configuration.host
-        components.path = path
-        return components.url!
+
+        // Check if host contains a path component
+        let hostParts = configuration.host.split(separator: "/", maxSplits: 1, omittingEmptySubsequences: true)
+        if hostParts.count > 1 {
+            // Host contains a path component
+            components.host = String(hostParts[0])
+            let hostPath = "/" + hostParts[1]
+            components.path = hostPath + path
+        } else {
+            // Host does not contain a path component
+            components.host = configuration.host
+            components.path = path
+        }
+
+        guard let url = components.url else {
+            fatalError("Unable to construct URL from components.")
+        }
+
+        return url
     }
 }
 
 typealias APIPath = String
 extension APIPath {
-    
     static let completions = "/v1/completions"
     static let embeddings = "/v1/embeddings"
     static let chats = "/v1/chat/completions"
     static let edits = "/v1/edits"
     static let models = "/v1/models"
     static let moderations = "/v1/moderations"
-    
+
     static let audioSpeech = "/v1/audio/speech"
     static let audioTranscriptions = "/v1/audio/transcriptions"
     static let audioTranslations = "/v1/audio/translations"
-    
+
     static let images = "/v1/images/generations"
     static let imageEdits = "/v1/images/edits"
     static let imageVariations = "/v1/images/variations"
-    
+
     func withPath(_ path: String) -> String {
         self + "/" + path
     }
