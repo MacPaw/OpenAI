ad8026b8e945f02aabe5dd657a4c22bd7c421570
nezhyborets <am.nezhyborets@gmail.com>
2025-01-31 19:43:49 +0200

    Add import to fix error

--- Sources/OpenAI/Private/URLSessionCombine.swift
+++ Sources/OpenAI/Private/URLSessionCombine.swift
@@ -5,9 +5,10 @@
 //  Created by Oleksii Nezhyborets on 31.01.2025.
 //
 
+import Foundation
+
 #if canImport(Combine)
 import Combine
-import Foundation
 
 protocol URLSessionCombine {
     @available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.0, *)
