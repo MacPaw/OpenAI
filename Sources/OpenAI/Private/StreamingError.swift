8346168796a306b39cb634e681dc3deae33d9783
nezhyborets <am.nezhyborets@gmail.com>
2025-02-03 13:11:08 +0200

    Add stream interpreter and write tests

---
+++ Sources/OpenAI/Private/StreamingError.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  OpenAI
+//
+//  Created by Oleksii Nezhyborets on 03.02.2025.
+//
+
+import Foundation
+
+enum StreamingError: Error {
+    case unknownContent
+    case emptyContent
+}
