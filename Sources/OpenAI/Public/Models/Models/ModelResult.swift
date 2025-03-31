23b5aefc5ba4ddb96cd01b5a410cc6f4a3c463ad
nezhyborets <am.nezhyborets@gmail.com>
2025-03-31 16:06:27 +0300

    Consider parsingOptions when decoding ModelResult

--- Sources/OpenAI/Public/Models/Models/ModelResult.swift
+++ Sources/OpenAI/Public/Models/Models/ModelResult.swift
@@ -13,7 +13,7 @@ public struct ModelResult: Codable, Equatable, Sendable {
     /// The model identifier, which can be referenced in the API endpoints.
     public let id: String
     /// The Unix timestamp (in seconds) when the model was created.
-    public let created: TimeInterval?
+    public let created: TimeInterval
     /// The object type, which is always "model".
     public let object: String
     /// The organization that owns the model.
@@ -24,5 +24,21 @@ public struct ModelResult: Codable, Equatable, Sendable {
         case created
         case object
         case ownedBy = "owned_by"
+    }
+    
+    public init(id: String, created: TimeInterval, object: String, ownedBy: String) {
+        self.id = id
+        self.created = created
+        self.object = object
+        self.ownedBy = ownedBy
+    }
+    
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let parsingOptions = decoder.userInfo[.parsingOptions] as? ParsingOptions ?? []
+        self.id = try container.decode(String.self, forKey: .id)
+        self.created = try container.decodeTimeInterval(forKey: .created, parsingOptions: parsingOptions)
+        self.object = try container.decode(String.self, forKey: .object)
+        self.ownedBy = try container.decode(String.self, forKey: .ownedBy)
     }
 }
