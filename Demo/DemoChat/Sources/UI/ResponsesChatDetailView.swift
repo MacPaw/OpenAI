cf251d63f7e947a3eb96f7a621815be8749d2cca
nezhyborets <am.nezhyborets@gmail.com>
2025-04-13 21:24:13 +0300

    Show settings

--- Demo/DemoChat/Sources/UI/ResponsesChatDetailView.swift
+++ Demo/DemoChat/Sources/UI/ResponsesChatDetailView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 import ExyteChat
 import OpenAI
 
+class ResponsesSettingsStore: ObservableObject {
+    @Published var selectedModel: Model = .gpt4_o
+    @Published var stream = false
+    @Published var webSearchEnabled = true
+    
+    
+}
+
 public struct ResponsesChatDetailView: View {
     @ObservedObject var store: ResponsesStore
     
     @State private var errorTitle = ""
     @State private var errorAlertPresented = false
+    @State private var isSettingsDisplayed = false
     
     public init(store: ResponsesStore) {
         self.store = store
@@ -21,22 +30,7 @@ public struct ResponsesChatDetailView: View {
     
     public var body: some View {
         NavigationStack {
-            ExyteChat.ChatView(
-                messages: store.chatMessages,
-                chatType: .conversation,
-                replyMode: .answer,
-                didSendMessage: { draftMessage in
-                    Task {
-                        do {
-                            try await store.send(message: draftMessage, stream: true)
-                        } catch {
-                            errorTitle = error.localizedDescription
-                            errorAlertPresented = true
-                        }
-                    }
-            })
-            .setAvailableInputs([.text, .media])
-            .messageUseMarkdown(true)
+            chatView()
             .toolbar(content: {
                 ToolbarItem(placement: .principal) {
                     HStack(spacing: 8) {
@@ -46,13 +40,44 @@ public struct ResponsesChatDetailView: View {
                         } else if store.inProgress {
                             Text("Streaming…")
                         } else {
-                            Text("") // Or you could return Text("Ready") or similar
+                            Text("Responses API") // Or you could return Text("Ready") or similar
                         }
                     }
                 }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        isSettingsDisplayed = true
+                    } label: {
+                        Image(systemName: "gearshape")
+                    }
+
+                }
             })
             .navigationBarTitleDisplayMode(.inline)
-        }.alert(errorTitle, isPresented: $errorAlertPresented, actions: {})
+            .alert(errorTitle, isPresented: $errorAlertPresented, actions: {})
+            .sheet(isPresented: <#T##Binding<Bool>#>, content: <#T##() -> View#>)
+        }
+    }
+    
+    @ViewBuilder
+    private func chatView() -> some View {
+        ExyteChat.ChatView(
+            messages: store.chatMessages,
+            chatType: .conversation,
+            replyMode: .answer,
+            didSendMessage: { draftMessage in
+                Task {
+                    do {
+                        try await store.send(message: draftMessage, stream: true)
+                    } catch {
+                        errorTitle = error.localizedDescription
+                        errorAlertPresented = true
+                    }
+                }
+        })
+        .setAvailableInputs([.text, .media])
+        .messageUseMarkdown(true)
     }
 }
 
