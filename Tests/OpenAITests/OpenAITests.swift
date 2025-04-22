dc59eb16d9591678d1dd5035d84312b1e4a3431b
nezhyborets <am.nezhyborets@gmail.com>
2025-04-22 19:40:49 +0300

    Extract weather function tool mock to fix duplic

--- Tests/OpenAITests/OpenAITests.swift
+++ Tests/OpenAITests/OpenAITests.swift
@@ -179,26 +179,7 @@ class OpenAITests: XCTestCase {
             model: .gpt3_5Turbo,
             toolChoice: .auto,
             tools: [
-                .init(
-                    function: .init(
-                        name: "get_current_weather",
-                        description: "Get the current weather in a given location",
-                        parameters: .init(fields: [
-                            .type(.object),
-                            .properties([
-                                "location": .init(fields: [
-                                    .type(.string),
-                                    .description("The city and state, e.g. San Francisco, CA")
-                                ]),
-                                "unit": .init(fields: [
-                                    .type(.string),
-                                    .enumValues(["celsius", "fahrenheit"])
-                                ])
-                            ]),
-                            .required(["location"])
-                        ])
-                    )
-                )
+                .makeWeatherMock()
             ]
         )
 
--- Tests/OpenAITests/OpenAITestsDecoder.swift
+++ Tests/OpenAITests/OpenAITestsDecoder.swift
@@ -205,26 +205,7 @@ class OpenAITestsDecoder: XCTestCase {
             responseFormat: ChatQuery.ResponseFormat.jsonObject,
             toolChoice: .function("get_current_weather"),
             tools: [
-                .init(
-                    function: .init(
-                        name: "get_current_weather",
-                        description: "Get the current weather in a given location",
-                        parameters: .init(fields: [
-                            .type(.object),
-                            .properties([
-                                "location": .init(fields: [
-                                    .type(.string),
-                                    .description("The city and state, e.g. San Francisco, CA")
-                                ]),
-                                "unit": .init(fields: [
-                                    .type(.string),
-                                    .enumValues(["celsius", "fahrenheit"])
-                                ])
-                            ]),
-                            .required(["location"])
-                        ])
-                    )
-                )
+                .makeWeatherMock()
             ]
         )
         let expectedValue = """
