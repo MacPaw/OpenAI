4ca0591fc785471fb9adfcb31d14f59dc49afeda
SplittyDev <splittydev@protonmail.com>
2025-06-25 03:09:12 +0700

    Support OpenRouter models endpoint

--- Sources/OpenAI/Public/Models/Models/ModelsResult.swift
+++ Sources/OpenAI/Public/Models/Models/ModelsResult.swift
@@ -14,4 +14,16 @@ public struct ModelsResult: Codable, Equatable, Sendable {
     public let data: [ModelResult]
     /// The object type, which is always `list`
     public let object: String
+
+    public init(data: [ModelResult], object: String) {
+        self.data = data
+        self.object = object
+    }
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let parsingOptions = decoder.userInfo[.parsingOptions] as? ParsingOptions ?? []
+        self.data = try container.decode([ModelResult].self, forKey: .data)
+        self.object = try container.decode(String.self, forKey: .object, parsingOptions: parsingOptions, defaultValue: "list")
+    }
 }
