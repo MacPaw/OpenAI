381c0f175a2c22d1df35ebff66760eadc8afcc3d
J.Conover <conover.j@gmail.com>
2025-09-25 13:25:12 -0400

    Apply suggestion from @JaredConover

--- Sources/OpenAI/Private/Streaming/ModelResponseEventsStreamInterpreter.swift
+++ Sources/OpenAI/Private/Streaming/ModelResponseEventsStreamInterpreter.swift
@@ -59,7 +59,7 @@ final class ModelResponseEventsStreamInterpreter: @unchecked Sendable, StreamInt
         var eventType = finalEvent.eventType
 
         /// If the SSE `event` property is not specified by the provider service, our parser defaults it to "message" which is not a valid model response type.
-    /// In this case we check the `data.type` property for a valid model response type.
+        /// In this case we check the `data.type` property for a valid model response type.
         if eventType == "message" || eventType.isEmpty,
            let payloadEventType = finalEvent.getPayloadType() {
             eventType = payloadEventType
