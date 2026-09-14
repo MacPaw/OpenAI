16d4543cf89c6a419b0f7ba5c31afdbb05d9d919
Govind Yadav <gtech.govind2000@gmail.com>
2026-09-14 23:05:40 +0530

    Add provider configuration to Demo

--- Demo/DemoChat/Package.swift
+++ Demo/DemoChat/Package.swift
@@ -27,5 +27,10 @@ let package = Package(
             ],
             path: "Sources"
         ),
+        .testTarget(
+            name: "DemoChatTests",
+            dependencies: ["DemoChat"],
+            path: "Tests"
+        ),
     ]
 )
